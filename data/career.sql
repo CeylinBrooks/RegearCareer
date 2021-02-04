e6352ab1c7cece09343095a89b0412fc60122af4
Mark Duenas <markgdlduenas@gmail.com>
2021-02-04 14:31:27 -0800

    deleted user table

--- data/career.sql
+++ data/career.sql
@@ -1,39 +1,27 @@
-DROP TABLE IF EXISTS users;
+
 DROP TABLE IF EXISTS schools;
 DROP TABLE IF EXISTS meetups;
 DROP TABLE IF EXISTS jobs;
-
-CREATE TABLE users (
-  user_id SERIAL PRIMARY KEY,
-  name VARCHAR(255),
-  email VARCHAR(255)
-  zip INTEGER
-  career VARCHAR(255)
-);
 
 CREATE TABLE schools (
   school_id SERIAL PRIMARY KEY,
   name VARCHAR(255),
   type VARCHAR(255),
   description VARCHAR(255),
-  
-  tuition INTEGER,
-  user_id INTEGER REFERENCES users (user_id) 
+  tuition INTEGER
 );
 
 CREATE TABLE jobs (
   job_id SERIAL PRIMARY KEY,
   title VARCHAR(255),
   salary INTEGER,
-  seniority_level VARCHAR(255),
-  user_id INTEGER REFERENCES users (user_id) 
+  seniority_level VARCHAR(255)
 );
 
-CREATE TABLE meet_ups (
+CREATE TABLE meetups (
   meet_up_id SERIAL PRIMARY KEY,
   title VARCHAR(255),
   type VARCHAR(255),
   location VARCHAR(255),
-  date DATE,
-  user_id INTEGER REFERENCES users (user_id)
+  date DATE
 );
