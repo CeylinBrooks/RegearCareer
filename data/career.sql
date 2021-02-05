83864a5bb223eed8c140d188213a1de6b25bda47
Mark Duenas <63880279+MarkDuenas@users.noreply.github.com>
2021-02-04 19:15:14 -0800

    updated SQL

--- data/career.sql
+++ data/career.sql
@@ -1,23 +1,9 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> e01b66eaeda2c15efa786df27735faedafde4258
 DROP TABLE IF EXISTS schools;
 DROP TABLE IF EXISTS meetups;
 DROP TABLE IF EXISTS jobs;
 DROP TABLE IF EXISTS users CASCADE;
 
-<<<<<<< HEAD
-CREATE TABLE users (
-  user_id SERIAL PRIMARY KEY,
-  name VARCHAR(255),
-  email VARCHAR(255),
-  zip INTEGER,
-  career VARCHAR(255)
-);
 
-=======
->>>>>>> e01b66eaeda2c15efa786df27735faedafde4258
 CREATE TABLE schools (
   school_id SERIAL PRIMARY KEY,
   name VARCHAR(255),
